ad1c6144a0abf10f3e1366ffcdddb9529fe94145
Usman <108216726+baymax-01@users.noreply.github.com>
2023-05-17 06:37:41 +0500

    winform CRUD operation

---
+++ CRUD/EmpDetailsForm.Designer.cs
@@ -0,0 +1,342 @@
+﻿
+namespace CRUD
+{
+    partial class EmpDetailsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtage = new System.Windows.Forms.TextBox();
+            this.txtcontact = new System.Windows.Forms.TextBox();
+            this.btninsert = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.UploadImage = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmbgender = new System.Windows.Forms.ComboBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Emp ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Emp Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Emp Age";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Emp Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 18);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Emp Contact";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(39, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 18);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Emp Picture";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(140, 57);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(108, 20);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(140, 89);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(108, 20);
+            this.txtname.TabIndex = 1;
+            // 
+            // txtage
+            // 
+            this.txtage.Location = new System.Drawing.Point(140, 120);
+            this.txtage.Name = "txtage";
+            this.txtage.Size = new System.Drawing.Size(108, 20);
+            this.txtage.TabIndex = 2;
+            // 
+            // txtcontact
+            // 
+            this.txtcontact.Location = new System.Drawing.Point(140, 185);
+            this.txtcontact.Name = "txtcontact";
+            this.txtcontact.Size = new System.Drawing.Size(108, 20);
+            this.txtcontact.TabIndex = 4;
+            // 
+            // btninsert
+            // 
+            this.btninsert.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btninsert.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btninsert.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btninsert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btninsert.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btninsert.Location = new System.Drawing.Point(165, 379);
+            this.btninsert.Name = "btninsert";
+            this.btninsert.Size = new System.Drawing.Size(75, 27);
+            this.btninsert.TabIndex = 5;
+            this.btninsert.Text = "Insert";
+            this.btninsert.UseVisualStyleBackColor = true;
+            this.btninsert.Click += new System.EventHandler(this.btninsert_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnupdate.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnupdate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnupdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnupdate.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.Location = new System.Drawing.Point(84, 379);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 27);
+            this.btnupdate.TabIndex = 6;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btndelete.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btndelete.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btndelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndelete.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.Location = new System.Drawing.Point(165, 412);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 27);
+            this.btndelete.TabIndex = 7;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label7.Location = new System.Drawing.Point(32, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(200, 29);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Employee Details";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Location = new System.Drawing.Point(140, 220);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.UploadImage});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(149, 26);
+            // 
+            // UploadImage
+            // 
+            this.UploadImage.Name = "UploadImage";
+            this.UploadImage.Size = new System.Drawing.Size(148, 22);
+            this.UploadImage.Text = "Upload Image";
+            this.UploadImage.Click += new System.EventHandler(this.UploadImage_Click);
+            // 
+            // cmbgender
+            // 
+            this.cmbgender.FormattingEnabled = true;
+            this.cmbgender.Items.AddRange(new object[] {
+            "--Select Gender",
+            "Male",
+            "Female"});
+            this.cmbgender.Location = new System.Drawing.Point(140, 154);
+            this.cmbgender.Name = "cmbgender";
+            this.cmbgender.Size = new System.Drawing.Size(108, 21);
+            this.cmbgender.TabIndex = 3;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(288, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(500, 380);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick_1);
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsearch.Location = new System.Drawing.Point(412, 19);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(207, 29);
+            this.txtsearch.TabIndex = 1;
+            this.txtsearch.TextChanged += new System.EventHandler(this.searchtext_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(335, 27);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 18);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Search";
+            // 
+            // EmpDetailsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmbgender);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btninsert);
+            this.Controls.Add(this.txtcontact);
+            this.Controls.Add(this.txtage);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "EmpDetailsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtage;
+        private System.Windows.Forms.TextBox txtcontact;
+        private System.Windows.Forms.Button btninsert;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cmbgender;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem UploadImage;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ CRUD/EmpDetailsForm.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CRUD
+{
+    public partial class EmpDetailsForm : Form
+    {
+        public EmpDetailsForm()
+        {
+            InitializeComponent();
+            BindGridView();
+            pictureBox1.Image = Properties.Resources.OIP;
+            cmbgender.SelectedIndex = 0;
+        }
+        string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+        void BindGridView()
+        {
+
+            SqlConnection con = new SqlConnection(cs);
+            string query = "select * from EmpDetails ";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            dataGridView1.DataSource = data;
+            DataGridViewImageColumn dgv = new DataGridViewImageColumn();
+            dgv = (DataGridViewImageColumn)dataGridView1.Columns[5];
+            dgv.ImageLayout = DataGridViewImageCellLayout.Stretch;
+            dataGridView1.RowTemplate.Height = 50;
+        }
+        public void FillGrid(string searchtext)
+        {
+            int n;
+            bool isNumeric = int.TryParse(txtsearch.Text, out n);
+            string query = "";
+            SqlConnection con = new SqlConnection(cs);
+
+            try
+            {
+                query = string.Empty;
+
+                if (isNumeric == false)
+                {
+
+                    if (string.IsNullOrEmpty(searchtext.Trim()))
+                    {
+                        query = "select * from empdetails";
+                    }
+                    else
+                    {
+
+                        query = "select  id, name,age,gender,contact,picture from Empdetails where (name+ ' ' +gender) like'%" + searchtext.Trim() + "%'";
+                    }
+                }
+                if (isNumeric)
+                {
+                    if (string.IsNullOrEmpty(searchtext.Trim()))
+                    {
+                        query = "select * from empdetails";
+                    }
+                    else
+                    {
+                        query = "select  id, name,age,gender,contact,picture from Empdetails where (id+ ' ' +age+' '+contact) like'%" + searchtext.Trim() + "%'";
+
+                    }
+                }
+
+                SqlDataAdapter sda = new SqlDataAdapter(query, con);
+                DataTable data = new DataTable();
+                sda.Fill(data);
+                dataGridView1.DataSource = data;
+
+
+            }
+            catch
+            {
+
+                MessageBox.Show("Some Issue Occur ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btninsert_Click(object sender, EventArgs e)
+        {
+            if (cmbgender.SelectedIndex == 0)
+            {
+                errorProvider1.SetError(this.cmbgender, "Please Select Gender !");
+                cmbgender.Focus();
+                return;
+            }
+            SqlConnection con = new SqlConnection(cs);
+
+            string query = "insert into EmpDetails values (@name,@age,@gender,@contact,@pic)";
+            //string query = string.Format("insert into empdetails (name,age,gender,contact,picture) values('{0}','{1}','{2}','{3}','{4}')",
+            //    txtname.Text,txtage.Text,cmbgender.SelectedItem,txtcontact.Text,SavePhoto());
+            SqlCommand cmd = new SqlCommand(query, con);
+            //cmd.Parameters.AddWithValue("@id", txtID.Text);
+            cmd.Parameters.AddWithValue("@name", txtname.Text);
+            cmd.Parameters.AddWithValue("@age", txtage.Text);
+            cmd.Parameters.AddWithValue("@gender", cmbgender.Text);
+            cmd.Parameters.AddWithValue("@contact", txtcontact.Text);
+            cmd.Parameters.AddWithValue("@pic", SavePhoto());
+            con.Open();
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                MessageBox.Show("Inserted Data Successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BindGridView();
+                clearControls();
+            }
+            else
+            {
+                MessageBox.Show("Insertion Failed", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
+
+        }
+        void clearControls()
+        {
+            txtID.Text = string.Empty;
+            txtname.Text = string.Empty;
+            txtcontact.Text = string.Empty;
+            txtage.Text = string.Empty;
+            cmbgender.SelectedIndex = 0;
+            pictureBox1.Image = Properties.Resources.OIP;
+        }
+        private byte[] SavePhoto()
+        {
+            MemoryStream ms = new MemoryStream();
+            pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
+            return ms.GetBuffer();
+        }
+        private Image GetPhoto(byte[] img)
+        {
+            MemoryStream ms = new MemoryStream(img);
+            return Image.FromStream(ms);
+        }
+
+        private void UploadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "Upload Image";
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp|All files (*.*)|*.*";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(open.FileName);
+            }
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query = "delete from EmpDetails where ID = @ID";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@ID", txtID.Text);
+
+            con.Open();
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                MessageBox.Show("Deleted  Data Successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BindGridView();
+                clearControls();
+            }
+            else
+            {
+                MessageBox.Show("Deletion Failed Failed", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            if (cmbgender.SelectedIndex == 0)
+            {
+                errorProvider1.SetError(this.cmbgender, "Please Select Gender !");
+                cmbgender.Focus();
+                return;
+            }
+            SqlConnection con = new SqlConnection(cs);
+            string query = "update Empdetails set name=@name,age=@age,gender=@gender,contact=@contact,picture=@pic where id=@id  ";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", txtID.Text);
+            cmd.Parameters.AddWithValue("@name", txtname.Text);
+            cmd.Parameters.AddWithValue("@age", txtage.Text);
+            cmd.Parameters.AddWithValue("@gender", cmbgender.Text);
+            cmd.Parameters.AddWithValue("@contact", txtcontact.Text);
+            cmd.Parameters.AddWithValue("@pic", SavePhoto());
+            con.Open();
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                MessageBox.Show("Update Data Successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BindGridView();
+                clearControls();
+            }
+            else
+            {
+                MessageBox.Show("Updation Failed", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
+        }
+
+        private void dataGridView1_MouseDoubleClick_1(object sender, MouseEventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                txtID.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                txtname.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+                txtage.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+                cmbgender.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+                txtcontact.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+                pictureBox1.Image = GetPhoto((byte[])dataGridView1.SelectedRows[0].Cells[5].Value);
+            }
+        }
+
+        private void searchtext_TextChanged(object sender, EventArgs e)
+        {
+            FillGrid(txtsearch.Text.Trim());
+        }
+    }
+}
